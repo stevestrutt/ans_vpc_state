3b513a955650d00bc0027340ba4d5b6106419313
STEPHEN STRUTT <steve_strutt@uk.ibm.com>
2020-01-31 14:28:32 +0000

    new

--- main.tf
+++ main.tf
@@ -1,41 +1,29 @@
-resource "ibm_compute_ssh_key" "ssh_key" {
-  label      = "${var.ssh_label}"
-  notes      = "${var.ssh_notes}"
-  public_key = "${var.ssh_key}"
-}
-
-
 resource "null_resource" "null01" {
-  
   connection {
-    user = "root"
-    host = "host"
+    host        = "52.116.140.31"
+    user        = "root"
+    private_key = "${var.ssh_private_key}"
   }
+
   provisioner "ansible" {
     plays {
       playbook = {
         file_path = "${path.module}/ansible-data/playbooks/install-tree.yml"
+
         roles_path = [
-            "${path.module}/ansible-data/roles"
+          "${path.module}/ansible-data/roles",
         ]
       }
-      groups = ["webserver"]
-      inventory_file = "${path.module}/ansible-data/inventory"
+
+      #inventory_file = "${path.module}/ansible-data/inventory"
     }
+
     ansible_ssh_settings {
       insecure_no_strict_host_key_checking = "${var.insecure_no_strict_host_key_checking}"
     }
-  }    
-
-
-
+  }
 }
-
-
 
 variable "insecure_no_strict_host_key_checking" {
   default = false
 }
-
-
-      
