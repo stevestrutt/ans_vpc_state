3ef89f90518bd2455b9a5681b7f0677d498cbb4f
STEPHEN STRUTT <steve_strutt@uk.ibm.com>
2020-04-23 20:54:01 +0100

    latest

--- main.tf
+++ main.tf
@@ -1,59 +1,79 @@
-resource "null_resource" "null01" {
+
+
+
+locals {
+  inventory_file = <<-EOT
+
+    [frontend]
+    %{for ip in var.frontend_hosts~}
+    ${ip[0]} ansible_host=${ip[1]}
+    %{endfor}
+
+    [backend]
+    %{for ip in var.backend_hosts~}
+    ${ip[0]} ansible_host=${ip[1]} 
+    %{endfor}
+  EOT
+}
+
+
+resource "local_file" "ips" {
+  filename = "${path.module}/ansible-data/inventory"
+  content  = local.inventory_file
+}
+
+
+
+resource "null_resource" "ansible" {
   connection {
-    bastion_host = "${var.bastion_host}"
+    bastion_host = var.bastion_host
 
-    #host = "52.116.140.31"
+    host = "172.16.2.11"
+    #user = "root"
 
-    # host = "172.22.192.8"
-    user = "root"
-    #private_key = "${file("~/.ssh/ansible")}"
+    #bastion_host_key = "${file("~/.ssh/ansible")}"
 
-    private_key = "${var.ssh_private_key}"
+    private_key = "${file("~/.ssh/ansible")}"
   }
 
-  # provisioner "remote-exec" {
-  #   script = "list.sh"
-  # }
-
   triggers = {
-    always_run = "${timestamp()}"
+    always_run = timestamp()
   }
   provisioner "ansible" {
     plays {
-      playbook = {
-        file_path = "${path.module}/ansible-data/playbooks/install-tree.yml"
+      playbook {
+        file_path = "${path.module}/ansible-data/playbooks/site.yml"
 
         roles_path = [
           "${path.module}/ansible-data/roles",
         ]
       }
-
-      verbose = true
-      hosts   = ["${var.target_hosts}"]
-      #inventory_file = "${path.module}/ansible-data/inventory"
+      verbose        = true
+      inventory_file = "${path.module}/ansible-data/inventory"
     }
 
     ansible_ssh_settings {
-      insecure_no_strict_host_key_checking = "${var.insecure_no_strict_host_key_checking}"
+      insecure_no_strict_host_key_checking = var.insecure_no_strict_host_key_checking
       connect_timeout_seconds              = 60
-
-      #ssh_args                             = "-o ControlPersist=30m -o ControlMaster=auto"
     }
   }
 }
 
-variable "ssh_private_key" {
-  description = "private ssh key"
-}
-
 variable "insecure_no_strict_host_key_checking" {
-  default = false
+  default = true
 }
 
 variable "bastion_host" {
   description = "Bastion host public IP address"
+  default     = "52.116.128.158"
 }
 
-variable "target_hosts" {
+variable "frontend_hosts" {
   description = "List of private IP addresses of target hosts"
+  default     = [["host0", "172.16.2.12"], ["host1", "172.16.0.10"]]
+}
+
+variable "backend_hosts" {
+  description = "List of private IP addresses of target hosts"
+  default     = [["host3", "172.17.0.10"]]
 }
