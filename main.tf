f345ff72b79d35982f9887d0102abd0fd53a21cf
STEPHEN STRUTT <steve_strutt@uk.ibm.com>
2020-04-24 16:47:16 +0100

    latest

--- main.tf
+++ main.tf
@@ -9,10 +9,10 @@ locals {
     ${ip[0]} ansible_host=${ip[1]}
     %{endfor}
 
-    # [backend]
-    # %{for ip in var.backend_hosts~}
-    # ${ip} ansible_host=${ip} 
-    # %{endfor}
+    [backend]
+    %{for ip in var.backend_hosts~}
+    ${ip[0]} ansible_host=${ip[1]} 
+    %{endfor}
   EOT
 }
 
@@ -42,47 +42,48 @@ resource "null_resource" "ls2" {
     always_run = timestamp()
   }
   provisioner "local-exec" {
-    command = "ls -al ${path.module}/ansible-data/inventory; pwd"
+    command = "ls -al ${path.module}/ansible-data/inventory"
   }
+  depend_on = 
 }
 
 
 
-# resource "null_resource" "ansible" {
-#   connection {
-#     bastion_host = var.bastion_host
+resource "null_resource" "ansible" {
+  connection {
+    bastion_host = var.bastion_host
 
-#     host = "172.16.2.11"
-#     #user = "root"
+    host = "172.16.2.11"
+    #user = "root"
 
-#     #bastion_host_key = "${file("~/.ssh/ansible")}"
+    #bastion_host_key = "${file("~/.ssh/ansible")}"
 
-#     #private_key = "${file("~/.ssh/ansible")}"
-#     private_key = var.ssh_private_key
-#   }
+    #private_key = "${file("~/.ssh/ansible")}"
+    private_key = var.ssh_private_key
+  }
 
-#   triggers = {
-#     always_run = timestamp()
-#   }
-#   provisioner "ansible" {
-#     plays {
-#       playbook {
-#         file_path = "${path.module}/ansible-data/playbooks/site.yml"
+  triggers = {
+    always_run = timestamp()
+  }
+  provisioner "ansible" {
+    plays {
+      playbook {
+        file_path = "${path.module}/ansible-data/playbooks/site.yml"
 
-#         roles_path = [
-#           "${path.module}/ansible-data/roles",
-#         ]
-#       }
-#       verbose        = true
-#       inventory_file = "${path.module}/ansible-data/inventory"
-#     }
+        roles_path = [
+          "${path.module}/ansible-data/roles",
+        ]
+      }
+      verbose        = true
+      inventory_file = "${path.module}/ansible-data/inventory"
+    }
 
-#     ansible_ssh_settings {
-#       insecure_no_strict_host_key_checking = var.insecure_no_strict_host_key_checking
-#       connect_timeout_seconds              = 60
-#     }
-#   }
-# }
+    ansible_ssh_settings {
+      insecure_no_strict_host_key_checking = var.insecure_no_strict_host_key_checking
+      connect_timeout_seconds              = 60
+    }
+  }
+}
 
 variable "ssh_private_key" {
 }
@@ -99,11 +100,11 @@ variable "bastion_host" {
 variable "frontend_hosts" {
   type        = list(list(string))
   description = "List of private IP addresses of target hosts"
-  default     = [["host1", "172.16.2.12"], ["host2", "172.16.2.13"]]
+  default     = [["host1", "172.16.0.11"], ["host2", "172.16.2.13"]]
 }
 
 variable "backend_hosts" {
-  type        = list(string)
+  type        = list(list(string))
   description = "List of private IP addresses of target hosts"
-  default     = ["host4", "172.17.0.10"]
+  default     = [["host4", "172.17.0.11"]]
 }
