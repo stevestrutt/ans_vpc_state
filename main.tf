b51e2c4178c227e3a4a2c2ee3c7940131aa60b62
STEPHEN STRUTT <steve_strutt@uk.ibm.com>
2020-05-07 22:35:51 +0100

    initial

--- main.tf
+++ main.tf
@@ -1,19 +1,28 @@
+# provider block required with Schematics to set VPC region
+provider "ibm" {
+  ibmcloud_api_key = var.ibmcloud_api_key
+  version          = "~> 1.5"
+}
 
 
+data "ibm_schematics_workspace" "vpc" {
+  workspace_id = var.workspace_id
+}
 
-locals {
-  inventory_file = <<-EOT
+data "ibm_schematics_output" "vpc" {
+  workspace_id = var.workspace_id
+  template_id  = "${data.ibm_schematics_workspace.vpc.template_id.0}"
+}
 
-    [frontend]
-    %{for ip in var.frontend_hosts~}
-    ${ip[0]} ansible_host=${ip[1]}
-    %{endfor}
+data "ibm_schematics_state" "vpc" {
+  workspace_id = var.workspace_id
+  template_id  = "${data.ibm_schematics_workspace.vpc.template_id.0}"
+}
 
-    [backend]
-    %{for ip in var.backend_hosts~}
-    ${ip[0]} ansible_host=${ip[1]} 
-    %{endfor}
-  EOT
+
+resource "local_file" "terraform_source_state" {
+  filename = "${path.module}source.tfstate"
+  content  = data.ibm_schematics_state.vpc.state_store
 }
 
 
@@ -26,9 +35,20 @@ locals {
 #   }
 # }
 
-# output "file" {
-#   value = local.inventory_file
-# }
+output "vpc_output" {
+  value = data.ibm_schematics_output.vpc
+}
+
+
+locals {
+  mapout = tomap(data.ibm_schematics_output.vpc.output_values)
+  basts  = matchkeys(values(local.mapout), keys(local.mapout), ["bastion_host_ip_addresses.#"])
+}
+
+output "vpc_bastions" {
+  value = data.ibm_schematics_output.vpc.output_values["bastion_host_ip_addresses.#"]
+
+}
 
 # output "file_read" {
 #   value = data.local_file.input
@@ -36,10 +56,10 @@ locals {
 
 
 
-resource "local_file" "ips" {
-  filename = "${path.module}/ansible-data/inventory.txt"
-  content  = local.inventory_file
-}
+# resource "local_file" "ips" {
+#   filename = "${path.module}/ansible-data/inventory.txt"
+#   content  = local.inventory_file
+# }
 
 # data "local_file" "input" {
 #   filename   = "${path.module}/ansible-data/inventory.txt"
@@ -60,63 +80,69 @@ resource "local_file" "ips" {
 
 
 
-resource "null_resource" "ansible" {
-  connection {
-    bastion_host = var.bastion_host
+# resource "null_resource" "ansible" {
+#   connection {
+#     bastion_host = var.bastion_host
 
-    host = "172.16.2.11"
-    #user = "root"
+#     host = "172.16.2.11"
+#     #user = "root"
 
-    #bastion_host_key = "${file("~/.ssh/ansible")}"
+#     #bastion_host_key = "${file("~/.ssh/ansible")}"
 
-    #private_key = "${file("~/.ssh/ansible")}"
-    private_key = var.ssh_private_key
-  }
+#     private_key = "${file("~/.ssh/ansible")}"
+#     #private_key = var.ssh_private_key
+#   }
 
-  triggers = {
-    always_run = timestamp()
-  }
-  provisioner "ansible" {
-    plays {
-      playbook {
-        file_path = "${path.module}/ansible-data/playbooks/site.yml"
+#   triggers = {
+#     always_run = timestamp()
+#   }
+#   provisioner "ansible" {
+#     plays {
+#       playbook {
+#         file_path = "${path.module}/ansible-data/playbooks/site.yml"
 
-        roles_path = [
-          "${path.module}/ansible-data/roles",
-        ]
-      }
-      verbose        = true
-      inventory_file = "${path.module}/ansible-data/inventory.txt"
-    }
+#         roles_path = [
+#           "${path.module}/ansible-data/roles",
+#         ]
+#       }
+#       verbose        = true
+#       inventory_file = "${path.module}/terraform_hosts.py"
+#     }
 
-    ansible_ssh_settings {
-      insecure_no_strict_host_key_checking = var.insecure_no_strict_host_key_checking
-      connect_timeout_seconds              = 60
-    }
-  }
-  depends_on = [local_file.ips]
+#     ansible_ssh_settings {
+#       insecure_no_strict_host_key_checking = true
+#       connect_timeout_seconds              = 60
+#     }
+#   }
+#   depends_on = [local_file.terraform_source_state]
+# }
+
+# variable "ssh_private_key" {
+# }
+
+
+variable "ibmcloud_api_key" {
+  description = "IBM Cloud API key when run standalone"
 }
 
-variable "ssh_private_key" {
-}
-
-variable "insecure_no_strict_host_key_checking" {
-  default = true
+variable "workspace_id" {
+  description = "Id of the source Schematics Workspace for target VSIs"
+  default     = "ssh_vpc_modules_test-f0c370ee-45f2-4e"
 }
 
 variable "bastion_host" {
   description = "Bastion host public IP address"
-  default     = "52.116.132.36"
+  default     = "52.116.128.158"
 }
 
-variable "frontend_hosts" {
-  type        = list(list(string))
-  description = "List of private IP addresses of target hosts"
-  default     = [["host1", "172.16.0.5"], ["host2", "172.16.2.5"]]
-}
+# variable "frontend_hosts" {
+#   type        = list(list(string))
+#   description = "List of private IP addresses of target hosts"
+#   default     = [["host1", "172.16.0.5"], ["host2", "172.16.2.5"]]
+# }
 
-variable "backend_hosts" {
-  type        = list(list(string))
-  description = "List of private IP addresses of target hosts"
-  default     = [["host4", "172.17.0.4"]]
-}
+# variable "backend_hosts" {
+#   type        = list(list(string))
+#   description = "List of private IP addresses of target hosts"
+#   default     = [["host4", "172.17.0.4"]]
+# }
