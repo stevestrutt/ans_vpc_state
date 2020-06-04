6ce7ca69b148b12bb1d3816bac5d649e7a081228
STEPHEN STRUTT <steve_strutt@uk.ibm.com>
2020-06-04 17:48:41 +0100

    new

--- main.tf
+++ main.tf
@@ -1,9 +1,8 @@
-# provider block required with Schematics to set VPC region
+
 provider "ibm" {
   # ibmcloud_api_key = var.ibmcloud_api_key
   version = "~> 1.5.3"
 }
-
 
 data "ibm_schematics_workspace" "vpc" {
   workspace_id = var.workspace_id
@@ -26,66 +25,15 @@ resource "local_file" "terraform_source_state" {
 
 }
 
-
-# resource "null_resource" "ls" {
-#   triggers = {
-#     always_run = timestamp()
-#   }
-#   provisioner "local-exec" {
-#     command = "cat ${path.module}/ansible-data/inventory.txt"
-#   }
-# }
-
-# output "vpc_output" {
-#   value = data.ibm_schematics_output.vpc
-# }
-
-
-
-output "vpc_bastions" {
-  value = data.ibm_schematics_output.vpc.output_values["bastion_host_ip_address"]
+output "app_dns_hostname" {
+  value = data.ibm_schematics_output.vpc.output_values["app_dns_hostname"]
 
 }
-
-# output "file_read" {
-#   value = data.local_file.input
-# }
-
-
-
-# resource "local_file" "ips" {
-#   filename = "${path.module}/ansible-data/inventory.txt"
-#   content  = local.inventory_file
-# }
-
-# data "local_file" "input" {
-#   filename   = "${path.module}/ansible-data/inventory.txt"
-#   depends_on = [local_file.ips]
-# }
-
-
-# resource "null_resource" "ls1" {
-#   triggers = {
-#     always_run = timestamp()
-#   }
-#   provisioner "local-exec" {
-#     command = "ls -al ${path.module}/ansible-data/inventory.txt"
-#   }
-#   depends_on = [local_file.ips]
-# }
-
-
-
 
 resource "null_resource" "ansible" {
   connection {
     bastion_host = data.ibm_schematics_output.vpc.output_values["bastion_host_ip_address"]
-
-    host = "0.0.0.0"
-    #user = "root"
-
-    #bastion_host_key = "${file("~/.ssh/ansible")}"
-
+    host         = "0.0.0.0"
     #private_key = "${file("~/.ssh/ansible")}"
     private_key = var.ssh_private_key
   }
@@ -98,14 +46,11 @@ resource "null_resource" "ansible" {
       playbook {
         file_path = "${path.module}/ansible-data/playbooks/site.yml"
 
-        roles_path = [
-          "${path.module}/ansible-data/roles",
-        ]
+        roles_path = ["${path.module}/ansible-data/roles"]
       }
-      verbose        = true
       inventory_file = "${path.module}/terraform_inv.py"
+      verbose        = true
     }
-
     ansible_ssh_settings {
       insecure_no_strict_host_key_checking = true
       connect_timeout_seconds              = 60
@@ -132,14 +77,4 @@ variable "bastion_host" {
   default     = "52.116.128.217"
 }
 
-# variable "frontend_hosts" {
-#   type        = list(list(string))
-#   description = "List of private IP addresses of target hosts"
-#   default     = [["host1", "172.16.0.5"], ["host2", "172.16.2.5"]]
-# }
 
-# variable "backend_hosts" {
-#   type        = list(list(string))
-#   description = "List of private IP addresses of target hosts"
-#   default     = [["host4", "172.17.0.4"]]
-# }
